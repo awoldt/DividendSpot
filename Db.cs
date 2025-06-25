9a9c48c0ed728f0e3b253955d78411e66621a632
awoldt <abreckwoldt23@gmail.com>
2025-06-25 08:22:58 -0400

    removed google ads

--- Db.cs
+++ Db.cs
@@ -135,6 +135,15 @@ public class Db
       return @$"
         <?xml version=""1.0"" encoding=""UTF-8""?>
         <urlset xmlns=""http://www.sitemaps.org/schemas/sitemap/0.9"">
+          <url>
+            <loc>https://dividendspot.com</loc>
+          </url>
+          <url>
+            <loc>https://dividendspot.com/about</loc>
+          </url>
+          <url>
+            <loc>https://dividendspot.com/privacy</loc>
+          </url>
           {string.Join("\n", urlLocs)}
         </urlset>
       ";
